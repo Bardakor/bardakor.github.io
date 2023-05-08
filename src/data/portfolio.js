084cd5193e39f5b3cf614451efac7b3110d7e6e3
Liam Abourousse <liam-alexandre.abourousse@epita.fr>
2023-05-09 00:51:36 +0200

    images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title : 'Attack On Castle',
-        imgUrl : '/assets/AOC.jpg',
+        imgUrl : '/assets/siege-castle.jpg',
         stack: ['C# ', 'Unity'],
         link : 'https://aygoun.github.io/Attack-On-Castle-Web.github.io/',
     },
     {   
         title : 'AFIT (Artithmetics for IT)',
-        imgUrl : '/assets/ocaml2.jpg',
+        imgUrl : '/assets/What-is-Binary-Coding-Rendered-size-948x500-1.jpg',
         stack: ['OCAML ', 'Python'],
         link : 'https://github.com/Bardakor/AFIT-S12025'
     },
     {
         title : 'FA and REGEX',
-        imgUrl : '/assets/Automaton.jpg',
+        imgUrl : '/assets/49789957488_9a2246e71c_o.png',
         stack: ['Python ', 'C'],
         link : 'https://github.com/arthur-gtgn/FA-REGEX-Project',
     },
